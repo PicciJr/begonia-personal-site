1dcfe4ee721d40eb4c1a377bb3e39fe38b7fc0da
PicciJr <piccijunior2319@gmail.com>
2021-05-19 08:18:52 +0200

    layout about me page

--- pages/sobre-mi.vue
+++ pages/sobre-mi.vue
@@ -1,7 +1,24 @@
 <!-- About me page -->
 <template>
-  <div>
-    Begoña
+  <div class="flex flex-col items-center justify-center px-8 mx-auto text-md md:text-lg text-begonia-sec-gray">
+    <!-- Profile image -->
+    <img
+      src="https://cdn.domestika.org/c_fill,dpr_auto,f_auto,h_256,pg_1,t_base_params,w_256/v1574944295/avatars/001/463/838/1463838-original.jpg?1574944295"
+      alt=""
+      class="w-64 h-64 mt-16 rounded-full shadow-lg"
+    >
+    <!-- Biography text -->
+    <p class="my-8 text-left">
+      It is a long established fact that a reader will be distracted by the
+      readable content of a page when looking at its layout. The point of using
+      Lorem Ipsum is that it has a more-or-less normal distribution of letters,
+      as opposed to using 'Content here, content here', making it look like
+      readable English. Many desktop publishing packages and web page editors
+      now use Lorem Ipsum as their default model text, and a search for 'lorem
+      ipsum' will uncover many web sites still in their infancy. Various
+      versions have evolved over the years, sometimes by accident, sometimes on
+      purpose (injected humour and the like).
+    </p>
   </div>
 </template>
 
