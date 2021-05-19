892fdc9dc6a4afd85b083fdfff859eada3f69aa8
PicciJr <piccijunior2319@gmail.com>
2021-05-19 08:08:37 +0200

    add long description to product detail

--- pages/producto/_id.vue
+++ pages/producto/_id.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="px-4 mt-12 mb-8 text-begonia-sec-gray">
     <!-- Image carousel -->
-    <a-image-carousel :images="product.images" class="mb-4" :autoplay="3000" :hoverpause="true" />
+    <a-image-carousel
+      :images="product.images"
+      class="mb-4"
+      :autoplay="3000"
+      :hoverpause="true"
+    />
     <!-- Title & price -->
     <div class="flex flex-col mb-4">
-      <h3 class="text-xl font-bold md:text-3xl">
+      <h3 class="text-2xl font-bold md:text-4xl">
         {{ product.title }}
       </h3>
       <span class="text-lg font-medium md:text-xl">{{ product.price }} €</span>
@@ -12,9 +17,27 @@
     <!-- Product options -->
     <m-product-configuration-options :product="product" class="mb-4" />
     <!-- Add to cart -->
-    <a-button cta-text="Añadir al carrito" class="w-full px-4 py-1 uppercase bg-begonia-primary-purple" />
+    <a-button
+      cta-text="Añadir al carrito"
+      class="w-full px-4 py-1 mb-4 uppercase bg-begonia-primary-purple"
+    />
+    <!-- Product description -->
+    <div class="mb-16">
+      <h3 class="mb-4 text-xl font-bold md:text-3xl">
+        Descripción
+      </h3>
+      <div :class="['mb-4 overflow-hidden', longDescriptionHeight]">
+        {{ product.longDescription }}
+      </div>
+      <div class="flex justify-center">
+        <span class="px-4 py-1 text-sm font-bold text-white rounded-full w-max bg-begonia-sec-gray" @click="toggleLongDescriptionVisibility">{{ longDescriptionBadgeText }}</span>
+      </div>
+    </div>
     <!-- Related products -->
-    <o-similar-products class="mt-8" :similar-products="product.similarProducts" />
+    <o-similar-products
+      class="mt-8"
+      :similar-products="product.similarProducts"
+    />
     <!-- Sticky footer Add to cart -->
     <m-cart-sticky-footer class="w-full h-20" />
   </div>
@@ -39,48 +62,76 @@ export default Vue.extend({
   data () {
     /**
      * @param title: titulo del producto
+     * @param longDescription: descripcion del producto
      * @param price: precio del producto
      * @param type: [lamina, original, etc]
      * @param sizes: tamaños disponibles, si aplica
+     * @param images: array de imagenes a mostrar en el carrusel de producto
      * @param hasVariants: saber si un producto es configurable/el usuario puede elegir opciones de tamaño o similar
+     * @param similarProducts: array de productos similares vinculados al producto
      */
     interface IProduct {
-        title: string
-        price: number
-        type: string
-        sizes?: string[]
-        images: string[]
-        hasVariants?: boolean
-        similarProducts?: IProduct[],
+      title: string
+      longDescription: string
+      price: number
+      type: string
+      sizes?: string[]
+      images: string[]
+      hasVariants?: boolean
+      similarProducts?: IProduct[]
     }
 
     const product: IProduct = {
       title: 'Lamina 1',
-      price: 10.50,
+      longDescription:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      price: 10.5,
       type: 'lamina',
       sizes: ['A4', 'A2', 'A3'],
-      images: ['https://images.unsplash.com/photo-1571115764595-644a1f56a55c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=980&q=80',
+      images: [
         'https://images.unsplash.com/photo-1571115764595-644a1f56a55c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=980&q=80',
-        'https://images.unsplash.com/photo-1571115764595-644a1f56a55c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=980&q=80'],
+        'https://images.unsplash.com/photo-1571115764595-644a1f56a55c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=980&q=80',
+        'https://images.unsplash.com/photo-1571115764595-644a1f56a55c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=980&q=80'
+      ],
       hasVariants: true,
       similarProducts: [
         {
           title: 'Original 1',
-          price: 100.20,
+          longDescription: '',
+          price: 100.2,
           type: 'original',
-          images: ['https://images.unsplash.com/photo-1579783902614-a3fb3927b6a5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=983&q=80']
+          images: [
+            'https://images.unsplash.com/photo-1579783902614-a3fb3927b6a5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=983&q=80'
+          ]
         },
         {
           title: 'Original 2',
-          price: 90.60,
+          longDescription: '',
+          price: 90.6,
           type: 'original',
-          images: ['https://images.unsplash.com/photo-1579965342575-16428a7c8881?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=999&q=80']
+          images: [
+            'https://images.unsplash.com/photo-1579965342575-16428a7c8881?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=999&q=80'
+          ]
         }
       ]
     }
 
     return {
-      product
+      product,
+      isLongDescriptionVisible: false
+    }
+  },
+  computed: {
+    longDescriptionHeight (): string {
+      return this.isLongDescriptionVisible ? 'h-full' : 'h-20'
+    },
+    longDescriptionBadgeText (): string {
+      return this.isLongDescriptionVisible ? 'Leer menos' : 'Leer más'
+    }
+  },
+  methods: {
+    toggleLongDescriptionVisibility () {
+      this.isLongDescriptionVisible = !this.isLongDescriptionVisible
     }
   }
 })
