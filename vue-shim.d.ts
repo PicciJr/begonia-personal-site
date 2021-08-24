433f3c61bbbdd2a70a254c8bb4971cd43e00ef25
PicciJr <piccijunior2319@gmail.com>
2021-08-24 19:29:44 +0200

    add comment for ts build

--- vue-shim.d.ts
+++ vue-shim.d.ts
@@ -1,3 +1,6 @@
+/** File required for Typescript configuration and build up
+ * https://typescript.nuxtjs.org/guide/setup/
+ */
 declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
